007b9431b9200c69df9e3edff0ed8804388bf41e
Sumit <sumitsinghindia5@gmail.com>
2022-06-21 22:18:00 +0530

    prototype method

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ const age=[15,25,12,45];
 const adults=age.filter((age)=>  age>=18
 );
 console.log(adults);
+
+function person (name ,age) {
+this.age=age;
+this.name=name;
+};
+//this is prototype method
+person.prototype.speak=function(){
+    console.log(`Hi my name is ${this.name} and my age is${this.age}`)
+};
+const sumit=new person("sumit",22);
+sumit.speak();
+console.log(sumit);
+console.log(sumit.name);
+console.log(sumit.age);
